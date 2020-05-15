0edac8830565ee1ba3306a50dcc0974320cb65a8
fbjorkm <37158786+fbjorkm@users.noreply.github.com>
2020-05-15 15:22:37 +0200

    Update http.go

--- http.go
+++ http.go
@@ -16,7 +16,7 @@ func main() {
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+        fmt.Fprintf(os.Stdout, "I'm Version 1 %s\n", hostname)
  	fmt.Fprintf(w, "I'm %s\n", hostname)
     })
 
